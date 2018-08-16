4e21a9e748613851bcd894a0b2a667a1b42cc7c8
buckzorz <alexey.aisulov@gmail.com>
2018-08-16 20:15:38 +0300

    done and dusted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'testTask';
+  search (searchForm: NgForm){
+    console.log(searchForm)
+    if(searchForm.valid){
+      let url = 'https://' + searchForm.value.engine + searchForm.value.query.replace(/ /g, '+')
+      console.log(url)
+      window.location.href = url
+    } else {
+      alert(`${searchForm.value.query ? ' ' : 'you are missing query!'}
+${searchForm.value.engine ? ' ' : 'you didn\'t select an engine!'}`)
+    }
+    
+  }
 }
